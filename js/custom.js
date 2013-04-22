308fa7273737d9c856b88dba9011ccf083c8c2dc
Christopher Anderton <chris@tikidream.com>
2013-04-22 10:46:28 -0400

    Journal-specific title filtering

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 
 /* 
  * Use functions like this to call additional javascript files through js.This * way you don't have to add any additional code to the template 
-*/
+ */
 // (function() {
 //   var url = "https://raw.github.com/jquery/jquery-color/master/jquery.color.js";
 //   $.getScript(url, function() {
@@ -25,10 +25,23 @@
  * Trunctate the long title of "On Our Terms" to something more breadcrumb
  * friendly. 
  */
+
+// Long title
+fullTitle = 'On Our Terms: The Undergraduate Journal of the Athena Center for Leadership Studies at Barnard College';
+
+// Friendly title
+friendlyTitle = 'On Our Terms';
+
 $(document).ready(function(){
+	// Shorten the title in the breadcrumbs
 	$('#breadcrumb a').each(function() {
-	if( $(this).text() == 'On Our Terms: The Undergraduate Journal of the Athena Center for Leadership Studies at Barnard College' ) { 
-		$(this).text('On Our Terms');
-	}
-});
+		if( $(this).text() == fullTitle ) { 
+			$(this).text(friendlyTitle);
+		}
+	});
+
+	// Hide the page title if it's the journal title
+	if( $('#breadcrumb + h2').text() == fullTitle ) { 
+			$('#breadcrumb + h2').hide();
+		}
 });
